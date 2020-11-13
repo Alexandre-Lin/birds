f3945322c3c2d2e02980bea1508ac6669c1329e9
Alex <lin.alexandre.16@gmail.com>
2020-11-13 19:57:38 +0100

    init service and class response API (2)

--- angular/app/src/app/model/bird-response.model.ts
+++ angular/app/src/app/model/bird-response.model.ts
@@ -1,2 +1,12 @@
-export class BirdResponse.Model {
+/* Response class for the bird API */
+export interface IBirdResponse {
+        specie: string;
+        rate: number;
+}
+
+export class BirdResponse implements IBirdResponse {
+    constructor(
+        public specie:  string,
+        public rate: number
+    ) {}
 }
